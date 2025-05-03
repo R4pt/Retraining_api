936eaa1da412f8e070bfedfd9f0859dba07153f4
R4pt <jafetrojascar@gmail.com>
2025-05-02 22:24:43 -0600

    Se agrego la configuracion de deepsource

--- retraining.py
+++ retraining.py
@@ -18,8 +18,8 @@ df_cleaned = df_cleaned.with_columns(
     (pl.when(pl.col("loan") == "no").then(0).otherwise(1).cast(pl.Int8()).alias("loan")),
     (pl.when(pl.col("marital") == "divorced").then(0).otherwise(1).cast(pl.Int8()).alias("marital")),
     (pl.when(pl.col("education") == "secondary").then(0).otherwise(1).cast(pl.Int8()).alias("education")),
-    (pl.col("default").map_elements(lambda x: False if x.lower() == "no" else True, return_dtype=pl.Boolean)).cast(pl.Int8()).alias("default"),
-    (pl.col("deposit").map_elements(lambda x: False if x.lower() == "no" else True, return_dtype=pl.Boolean)).cast(pl.Int8()).alias("deposit"),
+    (pl.col("default").map_elements(lambda x: x.lower() != "no" , return_dtype=pl.Boolean)).cast(pl.Int8()).alias("default"),
+    (pl.col("deposit").map_elements(lambda x: x.lower() != "no", return_dtype=pl.Boolean)).cast(pl.Int8()).alias("deposit"),
     pl.when(pl.col("month") == "jan").then(1).when(pl.col("month") == "feb").then(2).when(pl.col("month") == "mar").then(3).when(pl.col("month") == "apr").then(4).when(pl.col("month") == "may").then(5)
       .when(pl.col("month") == "jun").then(6).when(pl.col("month") == "jul").then(7).when(pl.col("month") == "aug").then(8).when(pl.col("month") == "sep").then(9)
       .when(pl.col("month") == "oct").then(10).when(pl.col("month") == "nov").then(11).when(pl.col("month") == "dec").then(12).cast(pl.Int8()).alias("month")
